43e25a368d6e7b32f3790f632c13ec34aff5610a
Andrii Kohut <andrii.kohut@nomo.tech>
2023-05-04 12:52:52 +0300

    Practice 4

---
+++ Practical 4/task_1/src/projects/ProjectsPage.js
@@ -0,0 +1,20 @@
+import { MOCK_PROJECTS } from './MockProjects';
+import ProjectList from './ProjectList';
+import React, { Fragment, useState } from 'react';
+
+function ProjectsPage() {
+    const [projects, setProjects] = useState(MOCK_PROJECTS);
+    const saveProject = (project) => {
+        let updatedProjects = projects.map((p) => {
+           return p.id === project.id ? project : p;
+           });
+       setProjects(updatedProjects);
+    };
+    return ( <>
+    <h1>Projects</h1>
+            <ProjectList onSave={saveProject} projects={projects} />
+    </>
+ );
+}
+
+export default ProjectsPage;
